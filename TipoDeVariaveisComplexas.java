bb80469c6f21dbdf6ebe721ebfd7393e7808ee78
Alailton Nascimento <allayunascimento@gmail.com>
2022-08-02 16:35:39 -0300

    TipoDeVariaveisComplexas.java

---
+++ TipoDeVariaveisComplexas.java
@@ -0,0 +1,14 @@
+public class TipoDeVariaveisComplexas {
+
+
+    public static void main (String args[]){
+
+        int numero1 = 55;
+        Integer numero2 = (Integer) numero1;
+
+        System.out.println("Tipo Primitivo: " + numero1);
+        System.out.println("Casting explícito para o Tipo Wrapper: " + numero2);
+
+
+    }
+}
